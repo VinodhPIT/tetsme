e181dc09f49baa4b4ee1bca8fca9a4a0a115c01d
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-09-11 11:36:05 +0530

    fix/missingImages
    Added the missing  images

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,63 +1,103 @@
-import React from "react";
+import React from 'react'
 import Link from "next/link";
-import styles from "./header.module.css";
+import { Router } from 'next/router';
+ import { useRouter } from "next/router";
+ import { Figtree } from 'next/font/google'
+ const figtree = Figtree({
+     style: ['normal',],
+     weight: ['400', '700','900','600'],
+     subsets: ['latin'],
+   })
+   
+   
 
 
 
-const links = [
+
+export default function Header() {
+
  
 
-  {
-    id: 2,
-    title: "Styleguide",
-    url: "/styleguide",
-  },
-  {
-    id: 3,
-    title: "Dictionary",
-    url: "/dictionary",
-  },
-  {
-    id: 4,
-    title: "For Tattooartists",
-    url: "/fortattooartists",
-  },
-  {
-    id: 5,
-    title: "Join Artists",
-    url: "/joinartist",
-  },
+  const links = [
+    {
+      id: 1,
+      title: "Tattoo search",
+      url: "/tattoosearch",
+    },
+    {
+      id: 2,
+      title: "Styleguide",
+      url: "/styleguide",
+    },
+    {
+      id: 3,
+      title: "Dictionary",
+      url: "/dictionary",
+    },
+ 
+    {
+      id: 4,
+      title: "Klarna",
+      url: "/klarna",
+    },
+  
+  ];
 
 
-
-];
-
-const Navbar =  () => {
-
-
-
+  const router = useRouter()
   return (
-    <header>
-        <div className={styles.headConatiner}>
-
-       
-      <div className={styles.logo}>
-        <Link href="/" style={{"color":"#fff"}}>Logo.</Link>
-      </div>
-      <nav className={styles.navmenu}>
-       <ul className={styles.kk}>
-       {links.map((link) => (
-        <li key={link.id}><Link  href={link.url}>
-        {link.title}
-      </Link></li>  
-        ))}
-        
-        </ul>
-      </nav>
    
-      </div>
-    </header>
-  );
-};
 
-export default Navbar;
+      <header  className="header_wrapper">
+			<div  className={figtree.className} >
+				<div className="container">
+					<nav className="navbar">
+
+
+						<Link href={'/'} className="navbar_brand" >
+							<img className="logo" src="./inckd-logo.svg" alt=""/>
+						</Link>
+
+
+						<div className="nav_right_block">
+							<ul className="nav main_nav navbar_collapse collapse">
+
+
+                      {links.map((link) => (
+              <li key={link.id} className="nav_item">
+                <Link   href={link.url}>{link.title}</Link>
+              </li>
+            ))}
+
+
+							</ul>
+
+
+
+							<div className="header_right">
+
+
+
+								<button type="button"  onClick={()=>router.push('/fortattooartists')} className="btn btn_tattoo_art navbar_collapse collapse" >For Tattoo Artists</button>
+
+								<button className="navbar_toggler" type="button" data-toggle="collapse"
+									data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+									aria-label="Toggle navigation">
+									<span></span>
+									<span></span>
+									<span></span>
+								</button>
+					
+							</div>
+
+
+						</div>
+
+					</nav>
+				</div>
+			</div>
+		</header>	
+      
+    
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,17 +3,29 @@ import { useRouter } from "next/router";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import { GlobalStateProvider } from "@/context/Context";
+import { Figtree } from 'next/font/google'
+
+const figtree = Figtree({
+  style: ['normal',],
+  weight: ['400', '700','900','600'],
+  subsets: ['latin'],
+})
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
   const hideHeaderRoutes = ["/search"]; // Add the routes where you want to hide the header
   const shouldHideHeader = hideHeaderRoutes.includes(router.pathname);
 
+
+
+ 
+
+
   return (
     <>
       <GlobalStateProvider>
         {!shouldHideHeader && <Header />}
-        <main>
+        <main className={figtree.className}>
           <Component {...pageProps} />
         </main>
         <Footer />
