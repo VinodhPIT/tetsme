2445f7bf473b5d3bd3ea4305ad6d8056519ea7a9
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-09-13 13:16:38 +0530

    feature/artistPage

--- src/components/artistForm/form.jsx
+++ src/components/artistForm/form.jsx
@@ -1,10 +1,8 @@
-
-import React, { useState} from "react";
-
+import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
 import * as Yup from "yup";
-
+import { countryList } from "@/constants/coutry";
+import { useRouter } from "next/router";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
@@ -18,139 +16,204 @@ const validationSchema = Yup.object().shape({
   location: Yup.string().required("Location field is required"),
 });
 
-const dropdownOptions = [
-
-  { value: "US", label: "Newyork" },
-  { value: "london", label: "London" },
-  { value: "mexico", label: "Mexico" },
-];
-
-
-
-
-
 const _Form = () => {
-
   const [state, setState] = useState({
-    successMessage: false,
-    loader:false
+    isShown: false,
   });
 
+const router = useRouter()
+
+
+
+  const onSubmit = () => {
+    router.push('/')
+  };
 
   return (
-    <div style={{ margin: "0 auto", padding: "40px" }}>
-      <Formik
-        initialValues={{
-          name: "",
-          email: "",
-          city: "",
-          instagram: "",
-          location: "",
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { resetForm }) => {
-          setState((prevSearchState) => ({
-            ...prevSearchState,
-            loader:true
-          }));
+    <div style={{ margin: "0 auto", padding: "0px" }}>
+      {state.isShown === false ? (
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            city: "",
+            instagram: "",
+            location: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { resetForm }) => {
+           
 
-          
-          fetch(
-            `https://admin.inckd.com/api/profile/artist/verification/request`,
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                first_name: values.name,
-                last_name: "",
-                email: values.email,
-                // country: values.location, 932692
-                country: 932692,
-                city_name: values.city,
-                instagram: values.instagram,
-                source: "web",
-              }),
-            }
-          )
-            .then((response) => {
-
-              if (response.ok) { 
+            fetch(
+              `https://admin.inckd.com/api/profile/artist/verification/request`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  first_name: values.name,
+                  last_name: "",
+                  email: values.email,
+                  country: values.location,
+                  city_name: values.city,
+                  instagram: values.instagram,
+                  source: "web",
+                }),
+              }
+            )
+              .then((e) => {
                 setState((prevSearchState) => ({
                   ...prevSearchState,
-                  success:true,
-                  loader:false
+                  isShown: true,
                 }));
-              }
-            })
-            .catch((e) => console.log(e));
 
-          resetForm();
-        }}
-      >
-        {({
-          values,
-          handleChange,
-          handleSubmit,
-          setFieldValue,
-          touched,
-          errors,
-        }) => (
-          <Form>
-            <div className="inputField">
-              {/* <label>Email:</label> */}
-              <Field type="name" name="name" placeholder="name" />
-              <ErrorMessage name="name" component="div" className="error" />
-            </div>
-
-            <Field component="select" id="location" name="location">
-              <option value="">Location</option>
-
-              {dropdownOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </Field>
-
-            <ErrorMessage name="location" component="div" className="error" />
-
-            
-
-            <div className="inputField">
-              {/* <label>Email:</label> */}
-              <Field type="email" name="email" placeholder="Email" />
-              <ErrorMessage name="email" component="div" className="error" />
-            </div>
-
-            <div className="inputField">
-              {/* <label>Message:</label> */}
-              <Field type="city" name="city" placeholder="city" />
-              <ErrorMessage name="city" component="div" className="error" />
-            </div>
-
-            <div className="inputField">
-              {/* <label>Message:</label> */}
-              <Field
-                type="instagram"
-                name="instagram"
-                placeholder="Instagram"
-              />
-              <ErrorMessage
-                name="instagram"
-                component="div"
-                className="error"
-              />
-            </div>
-            <button type="submit">Submit</button>
-
-          </Form>
-        )}
-      </Formik>
-
-=
-      
+                resetForm();
+              })
+              .catch((e) => console.log(e));
+          }}
+        >
+          {({ errors, isSubmitting }) => (
+            <Form class="form_floating">
+              <div class="form_block">
+                <div className="form_group">
+                  {/* <label>Email:</label> */}
+                  <Field
+                    type="name"
+                    name="name"
+                    placeholder=""
+                    className="form_control"
+                  />
+                  <label for="name" class="required">
+                    First name
+                  </label>
+                  <ErrorMessage name="name" component="div" className="error" />
+                </div>
+              </div>
+              <div class="form_block">
+                <div className="form_group">
+                  {/* <label>Email:</label> */}
+                  <Field
+                    type="email"
+                    name="email"
+                    placeholder=""
+                    className="form_control"
+                  />
+                  <label for="your e-mail" className="required">
+                    Your e-mail
+                  </label>
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+              </div>
+              <div class="form_block">
+                <div className="form_group">
+                  <div className="form_selec_wrap">
+                    <Field
+                      component="select"
+                      id="location"
+                      name="location"
+                      className="form_control_select"
+                    >
+                      <option value="">Location</option>
+                      {countryList.map((option) => (
+                        <option key={option.country} value={option.country}>
+                          {option.title}
+                        </option>
+                      ))}
+                    </Field>
+                  </div>
+                  <ErrorMessage
+                    name="location"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+              </div>
+              <div class="form_block">
+                <div className="form_group">
+                  {/* <label>Message:</label> */}
+                  <Field
+                    type="city"
+                    name="city"
+                    placeholder=""
+                    className="form_control"
+                  />
+                  <label for="City" className="required city">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                    >
+                      <path
+                        d="M12.7568 2.12671C8.88684 2.12671 5.75684 5.25671 5.75684 9.12671C5.75684 14.3767 12.7568 22.1267 12.7568 22.1267C12.7568 22.1267 19.7568 14.3767 19.7568 9.12671C19.7568 5.25671 16.6268 2.12671 12.7568 2.12671ZM12.7568 11.6267C11.3768 11.6267 10.2568 10.5067 10.2568 9.12671C10.2568 7.74671 11.3768 6.62671 12.7568 6.62671C14.1368 6.62671 15.2568 7.74671 15.2568 9.12671C15.2568 10.5067 14.1368 11.6267 12.7568 11.6267Z"
+                        fill="#707070"
+                      ></path>
+                    </svg>
+                    City
+                  </label>
+                  <ErrorMessage name="city" component="div" className="error" />
+                </div>
+              </div>
+              <div class="form_block">
+                <div className="form_group">
+                  {/* <label>Message:</label> */}
+                  <Field
+                    type="instagram"
+                    name="instagram"
+                    placeholder=""
+                    className="form_control"
+                  />
+                  <label for="Your instagram" className="required">
+                    Your instagram
+                  </label>
+                  <ErrorMessage
+                    name="instagram"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+              </div>
+              <div class="form_block">
+                <div className="form_group">
+                  <button
+                    type="submit"
+                    class="btn_secondary h_48 w_100pc"
+                    disabled={isSubmitting}
+                    style={{ opacity: isSubmitting ? 0.5 : 1 }}
+                  >
+                    {isSubmitting ? "Submitting ...." : "submit"}
+                  </button>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      ) : (
+        state.isShown && (
+          <div class="form_submit_block hidden">
+            <img src="./success-tick.svg" alt="Form submitted" />
+            <h4>Form submitted</h4>
+            <p>
+              Thank you!
+              <br />
+              Text need to be updated...
+            </p>
+            <button
+              type="submit"
+              class="btn_outline_secondary w_100pc h_48"
+              onClick={() => onSubmit()}
+            >
+              Ok.got it !
+            </button>
+          </div>
+        )
+      )}
     </div>
   );
 };
---
+++ src/constants/coutry.js
@@ -0,0 +1,907 @@
+export const countryList = [
+    {
+      country: 1149361,
+      title: 'Afghanistan',
+    },
+    {
+      country: 783754,
+      title: 'Albania',
+    },
+    {
+      country: 2589581,
+      title: 'Algeria',
+    },
+    {
+      country: 5880801,
+      title: 'American Samoa',
+    },
+    {
+      country: 3041565,
+      title: 'Andorra',
+    },
+    {
+      country: 3351879,
+      title: 'Angola',
+    },
+    {
+      country: 3573511,
+      title: 'Anguilla',
+    },
+    {
+      country: 3865483,
+      title: 'Argentina',
+    },
+    {
+      country: 174982,
+      title: 'Armenia',
+    },
+    {
+      country: 3577279,
+      title: 'Aruba',
+    },
+    {
+      country: 2077456,
+      title: 'Australia',
+    },
+    {
+      country: 2782113,
+      title: 'Austria',
+    },
+    {
+      country: 587116,
+      title: 'Azerbaijan',
+    },
+    {
+      country: 3572887,
+      title: 'Bahamas',
+    },
+    {
+      country: 290291,
+      title: 'Bahrain',
+    },
+    {
+      country: 1210997,
+      title: 'Bangladesh',
+    },
+    {
+      country: 3374084,
+      title: 'Barbados',
+    },
+    {
+      country: 630336,
+      title: 'Belarus',
+    },
+    {
+      country: 2802361,
+      title: 'Belgium',
+    },
+    {
+      country: 3582678,
+      title: 'Belize',
+    },
+    {
+      country: 2395170,
+      title: 'Benin',
+    },
+    {
+      country: 3573345,
+      title: 'Bermuda',
+    },
+    {
+      country: 1252634,
+      title: 'Bhutan',
+    },
+    {
+      country: 3923057,
+      title: 'Bolivia',
+    },
+    {
+      country: 7626844,
+      title: 'Bonaire',
+    },
+    {
+      country: 3277605,
+      title: 'Bosnia & Herzegovina',
+    },
+    {
+      country: 933860,
+      title: 'Botswana',
+    },
+    {
+      country: 3469034,
+      title: 'Brazil',
+    },
+    {
+      country: 1282588,
+      title: 'British Indian Ocean Territory',
+    },
+    {
+      country: 3577718,
+      title: 'British Virgin Islands',
+    },
+    {
+      country: 1820814,
+      title: 'Brunei',
+    },
+    {
+      country: 732800,
+      title: 'Bulgaria',
+    },
+    {
+      country: 2361809,
+      title: 'Burkina Faso',
+    },
+    {
+      country: 433561,
+      title: 'Burundi',
+    },
+    {
+      country: 1831722,
+      title: 'Cambodia',
+    },
+    {
+      country: 2233387,
+      title: 'Cameroon',
+    },
+    {
+      country: 6251999,
+      title: 'Canada',
+    },
+    {
+      country: 3374766,
+      title: 'Cape Verde',
+    },
+    {
+      country: 3580718,
+      title: 'Cayman Islands',
+    },
+    {
+      country: 239880,
+      title: 'Central African Republic',
+    },
+    {
+      country: 2434508,
+      title: 'Chad',
+    },
+    {
+      country: 3895114,
+      title: 'Chile',
+    },
+    {
+      country: 2078138,
+      title: 'Christmas Island',
+    },
+    {
+      country: 1547376,
+      title: 'Cocos (Keeling) Islands',
+    },
+    {
+      country: 3686110,
+      title: 'Colombia',
+    },
+    {
+      country: 921929,
+      title: 'Comoros',
+    },
+    {
+      country: 203312,
+      title: 'Congo (DRC)',
+    },
+    {
+      country: 2260494,
+      title: 'Congo (Republic)',
+    },
+    {
+      country: 1899402,
+      title: 'Cook Islands',
+    },
+    {
+      country: 3624060,
+      title: 'Costa Rica',
+    },
+    {
+      country: 3202326,
+      title: 'Croatia',
+    },
+    {
+      country: 7626836,
+      title: 'Cura\u00e7ao',
+    },
+    {
+      country: 146669,
+      title: 'Cyprus',
+    },
+    {
+      country: 3077311,
+      title: 'Czech Republic',
+    },
+    {
+      country: 2287781,
+      title: 'C\u00f4te d\u2019Ivoire',
+    },
+    {
+      country: 2623032,
+      title: 'Denmark',
+    },
+    {
+      country: 223816,
+      title: 'Djibouti',
+    },
+    {
+      country: 3575830,
+      title: 'Dominica',
+    },
+    {
+      country: 3508796,
+      title: 'Dominican Republic',
+    },
+    {
+      country: 3658394,
+      title: 'Ecuador',
+    },
+    {
+      country: 357994,
+      title: 'Egypt',
+    },
+    {
+      country: 3585968,
+      title: 'El Salvador',
+    },
+    {
+      country: 2309096,
+      title: 'Equatorial Guinea',
+    },
+    {
+      country: 338010,
+      title: 'Eritrea',
+    },
+    {
+      country: 453733,
+      title: 'Estonia',
+    },
+    {
+      country: 337996,
+      title: 'Ethiopia',
+    },
+    {
+      country: 3474414,
+      title: 'Falkland Islands (Islas Malvinas)',
+    },
+    {
+      country: 2622320,
+      title: 'Faroe Islands',
+    },
+    {
+      country: 2205218,
+      title: 'Fiji',
+    },
+    {
+      country: 660013,
+      title: 'Finland',
+    },
+    {
+      country: 3017382,
+      title: 'France',
+    },
+    {
+      country: 4030656,
+      title: 'French Polynesia',
+    },
+    {
+      country: 2400553,
+      title: 'Gabon',
+    },
+    {
+      country: 2413451,
+      title: 'Gambia',
+    },
+    {
+      country: 614540,
+      title: 'Georgia',
+    },
+    {
+      country: 2921044,
+      title: 'Germany',
+    },
+    {
+      country: 2300660,
+      title: 'Ghana',
+    },
+    {
+      country: 2411586,
+      title: 'Gibraltar',
+    },
+    {
+      country: 390903,
+      title: 'Greece',
+    },
+    {
+      country: 3425505,
+      title: 'Greenland',
+    },
+    {
+      country: 3580239,
+      title: 'Grenada',
+    },
+    {
+      country: 4043988,
+      title: 'Guam',
+    },
+    {
+      country: 3595528,
+      title: 'Guatemala',
+    },
+    {
+      country: 3042362,
+      title: 'Guernsey',
+    },
+    {
+      country: 2420477,
+      title: 'Guinea',
+    },
+    {
+      country: 2372248,
+      title: 'Guinea-Bissau',
+    },
+    {
+      country: 3378535,
+      title: 'Guyana',
+    },
+    {
+      country: 3723988,
+      title: 'Haiti',
+    },
+    {
+      country: 3608932,
+      title: 'Honduras',
+    },
+    {
+      country: 1819730,
+      title: 'Hong Kong',
+    },
+    {
+      country: 719819,
+      title: 'Hungary',
+    },
+    {
+      country: 2629691,
+      title: 'Iceland',
+    },
+    {
+      country: 1269750,
+      title: 'India',
+    },
+    {
+      country: 1643084,
+      title: 'Indonesia',
+    },
+    {
+      country: 99237,
+      title: 'Iraq',
+    },
+    {
+      country: 2963597,
+      title: 'Ireland',
+    },
+    {
+      country: 3042225,
+      title: 'Isle of Man',
+    },
+    {
+      country: 294640,
+      title: 'Israel',
+    },
+    {
+      country: 3175395,
+      title: 'Italy',
+    },
+    {
+      country: 3489940,
+      title: 'Jamaica',
+    },
+    {
+      country: 1861060,
+      title: 'Japan',
+    },
+    {
+      country: 3042142,
+      title: 'Jersey',
+    },
+    {
+      country: 248816,
+      title: 'Jordan',
+    },
+    {
+      country: 1522867,
+      title: 'Kazakhstan',
+    },
+    {
+      country: 192950,
+      title: 'Kenya',
+    },
+    {
+      country: 4030945,
+      title: 'Kiribati',
+    },
+    {
+      country: 831053,
+      title: 'Kosovo',
+    },
+    {
+      country: 285570,
+      title: 'Kuwait',
+    },
+    {
+      country: 1527747,
+      title: 'Kyrgyzstan',
+    },
+    {
+      country: 1655842,
+      title: 'Laos',
+    },
+    {
+      country: 458258,
+      title: 'Latvia',
+    },
+    {
+      country: 272103,
+      title: 'Lebanon',
+    },
+    {
+      country: 932692,
+      title: 'Lesotho',
+    },
+    {
+      country: 2275384,
+      title: 'Liberia',
+    },
+    {
+      country: 2215636,
+      title: 'Libya',
+    },
+    {
+      country: 3042058,
+      title: 'Liechtenstein',
+    },
+    {
+      country: 597427,
+      title: 'Lithuania',
+    },
+    {
+      country: 2960313,
+      title: 'Luxembourg',
+    },
+    {
+      country: 1821275,
+      title: 'Macau',
+    },
+    {
+      country: 718075,
+      title: 'Macedonia',
+    },
+    {
+      country: 1062947,
+      title: 'Madagascar',
+    },
+    {
+      country: 927384,
+      title: 'Malawi',
+    },
+    {
+      country: 1733045,
+      title: 'Malaysia',
+    },
+    {
+      country: 1282028,
+      title: 'Maldives',
+    },
+    {
+      country: 2453866,
+      title: 'Mali',
+    },
+    {
+      country: 2562770,
+      title: 'Malta',
+    },
+    {
+      country: 2080185,
+      title: 'Marshall Islands',
+    },
+    {
+      country: 3570311,
+      title: 'Martinique',
+    },
+    {
+      country: 2378080,
+      title: 'Mauritania',
+    },
+    {
+      country: 934292,
+      title: 'Mauritius',
+    },
+    {
+      country: 3996063,
+      title: 'Mexico',
+    },
+    {
+      country: 2081918,
+      title: 'Micronesia',
+    },
+    {
+      country: 617790,
+      title: 'Moldova',
+    },
+    {
+      country: 2993457,
+      title: 'Monaco',
+    },
+    {
+      country: 2029969,
+      title: 'Mongolia',
+    },
+    {
+      country: 3194884,
+      title: 'Montenegro',
+    },
+    {
+      country: 3578097,
+      title: 'Montserrat',
+    },
+    {
+      country: 2542007,
+      title: 'Morocco',
+    },
+    {
+      country: 3355338,
+      title: 'Mozambique',
+    },
+    {
+      country: 1327865,
+      title: 'Myanmar',
+    },
+    {
+      country: 3355338,
+      title: 'Namibia',
+    },
+    {
+      country: 2110425,
+      title: 'Nauru',
+    },
+    {
+      country: 1282988,
+      title: 'Nepal',
+    },
+    {
+      country: 2750405,
+      title: 'Netherlands',
+    },
+    {
+      country: 2186224,
+      title: 'New Zealand',
+    },
+    {
+      country: 3617476,
+      title: 'Nicaragua',
+    },
+    {
+      country: 2440476,
+      title: 'Niger',
+    },
+    {
+      country: 2328926,
+      title: 'Nigeria',
+    },
+    {
+      country: 4036232,
+      title: 'Niue',
+    },
+    {
+      country: 2155115,
+      title: 'Norfolk Island',
+    },
+    {
+      country: 1873107,
+      title: 'North Korea',
+    },
+    {
+      country: 4041468,
+      title: 'Northern Mariana Islands',
+    },
+    {
+      country: 3144096,
+      title: 'Norway',
+    },
+    {
+      country: 286963,
+      title: 'Oman',
+    },
+    {
+      country: 1168579,
+      title: 'Pakistan',
+    },
+    {
+      country: 1559582,
+      title: 'Palau',
+    },
+    {
+      country: 6254930,
+      title: 'Palestine',
+    },
+    {
+      country: 3703430,
+      title: 'Panama',
+    },
+    {
+      country: 2088628,
+      title: 'Papua New Guinea',
+    },
+    {
+      country: 3437598,
+      title: 'Paraguay',
+    },
+    {
+      country: 3932488,
+      title: 'Peru',
+    },
+    {
+      country: 1694008,
+      title: 'Philippines',
+    },
+    {
+      country: 4030699,
+      title: 'Pitcairn Islands',
+    },
+    {
+      country: 798544,
+      title: 'Poland',
+    },
+    {
+      country: 2264397,
+      title: 'Portugal',
+    },
+    {
+      country: 4566966,
+      title: 'Puerto Rico',
+    },
+    {
+      country: 289688,
+      title: 'Qatar',
+    },
+    {
+      country: 798549,
+      title: 'Romania',
+    },
+    {
+      country: 2017370,
+      title: 'Russia',
+    },
+    {
+      country: 49518,
+      title: 'Rwanda',
+    },
+    {
+      country: 4034894,
+      title: 'Samoa',
+    },
+    {
+      country: 3168068,
+      title: 'San Marino',
+    },
+    {
+      country: 102358,
+      title: 'Saudi Arabia',
+    },
+    {
+      country: 2245662,
+      title: 'Senegal',
+    },
+    {
+      country: 6290252,
+      title: 'Serbia',
+    },
+    {
+      country: 241170,
+      title: 'Seychelles',
+    },
+    {
+      country: 2403846,
+      title: 'Sierra Leone',
+    },
+    {
+      country: 1880251,
+      title: 'Singapore',
+    },
+    {
+      country: 7609695,
+      title: 'Sint Maarten',
+    },
+    {
+      country: 3057568,
+      title: 'Slovakia',
+    },
+    {
+      country: 3190538,
+      title: 'Slovenia',
+    },
+    {
+      country: 2103350,
+      title: 'Solomon Islands',
+    },
+    {
+      country: 51537,
+      title: 'Somalia',
+    },
+    {
+      country: 953987,
+      title: 'South Africa',
+    },
+    {
+      country: 1835841,
+      title: 'South Korea',
+    },
+    {
+      country: 7909807,
+      title: 'South Sudan',
+    },
+    {
+      country: 2510769,
+      title: 'Spain',
+    },
+    {
+      country: 1227603,
+      title: 'Sri Lanka',
+    },
+    {
+      country: 3578476,
+      title: 'St. Barth\u00e9lemy',
+    },
+    {
+      country: 3575174,
+      title: 'St. Kitts & Nevis',
+    },
+    {
+      country: 3576468,
+      title: 'St. Lucia',
+    },
+    {
+      country: 3577815,
+      title: 'St. Vincent & Grenadines',
+    },
+    {
+      country: 3382998,
+      title: 'Suriname',
+    },
+    {
+      country: 934841,
+      title: 'Swaziland',
+    },
+    {
+      country: 2661886,
+      title: 'Sweden',
+    },
+    {
+      country: 2658434,
+      title: 'Switzerland',
+    },
+    {
+      country: 2410758,
+      title: 'S\u00e3o Tom\u00e9 & Pr\u00edncipe',
+    },
+    {
+      country: 1668284,
+      title: 'Taiwan',
+    },
+    {
+      country: 1220409,
+      title: 'Tajikistan',
+    },
+    {
+      country: 149590,
+      title: 'Tanzania',
+    },
+    {
+      country: 1605651,
+      title: 'Thailand',
+    },
+    {
+      country: 1966436,
+      title: 'Timor-Leste',
+    },
+    {
+      country: 2363686,
+      title: 'Togo',
+    },
+    {
+      country: 4031074,
+      title: 'Tokelau',
+    },
+    {
+      country: 4032283,
+      title: 'Tonga',
+    },
+    {
+      country: 3573591,
+      title: 'Trinidad & Tobago',
+    },
+    {
+      country: 2464461,
+      title: 'Tunisia',
+    },
+    {
+      country: 298795,
+      title: 'Turkey',
+    },
+    {
+      country: 1218197,
+      title: 'Turkmenistan',
+    },
+    {
+      country: 2110297,
+      title: 'Tuvalu',
+    },
+    {
+      country: 4796775,
+      title: 'U.S. Virgin Islands',
+    },
+    {
+      country: 226074,
+      title: 'Uganda',
+    },
+    {
+      country: 690791,
+      title: 'Ukraine',
+    },
+    {
+      country: 290557,
+      title: 'United Arab Emirates',
+    },
+    {
+      country: 2635167,
+      title: 'United Kingdom',
+    },
+    {
+      country: 6252001,
+      title: 'United States',
+    },
+    {
+      country: 3439705,
+      title: 'Uruguay',
+    },
+    {
+      country: 1512440,
+      title: 'Uzbekistan',
+    },
+    {
+      country: 2134431,
+      title: 'Vanuatu',
+    },
+    {
+      country: 3164670,
+      title: 'Vatican City',
+    },
+    {
+      country: 3625428,
+      title: 'Venezuela',
+    },
+    {
+      country: 1562822,
+      title: 'Vietnam',
+    },
+    {
+      country: 69543,
+      title: 'Yemen',
+    },
+    {
+      country: 895949,
+      title: 'Zambia',
+    },
+    {
+      country: 878675,
+      title: 'Zimbabwe',
+    },
+    {
+      country: 661882,
+      title: '\u00c5land Islands',
+    },
+  ]
+  
