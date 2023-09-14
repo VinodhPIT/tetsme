ffccd8f68baa702c4b7d236a4f26d253612e017c
Binu Mohan <binu.mm@pitsolutions.com>
2023-09-14 21:00:38 +0530

    search page design update

--- src/components/tattoo/index.js
+++ src/components/tattoo/index.js
@@ -25,7 +25,9 @@ export default function Tattoo({ data }) {
             return item._index === "ad" ? (
               item.add===1 ?  <div className={styles.custom_adv_block_1}><div className={styles.custom_adv_wrap}><div className={styles.custom_adv_content}><h6>Find your<br/>Artist now!</h6>
               <span className={styles.adv_btn_wrap}><a href="#" className={styles.btn_secondary}>View Artists</a></span><img src="./pexels-cottonbro-studio-5320059-3.png" alt="View Artists" /></div></div></div> :
-              item.add===2  ?  <div className={styles.custom_adv_block_2}><p>Add 2 </p></div>  : <div className={styles.custom_adv_block_3}><p>Add 3 </p></div>
+              item.add===2  ?  <div className={styles.custom_adv_block_2}><div className={styles.custom_adv_wrap}><div className={styles.custom_adv_content}><img src="./klarna-b-19.svg" alt="Tattoo now Pay later" className={styles.adv_klarna}/><h6>Tattoo now.<br/>Pay later.</h6>
+              <span className={styles.adv_btn_wrap}><img src="./pngwing-39.png" alt="Tattoo now Pay later" className={styles.adv_img_app}/><img src="./pngwing-40.png" alt="Tattoo now Pay later" className={styles.adv_img_app}/></span><img src="./shutterstock_2068639577.png" alt="Tattoo now Pay later" /></div></div></div>  : 
+              <div className={styles.custom_adv_block_3}><div className={styles.custom_adv_wrap}><div className={styles.custom_adv_content}><h6>Get exciting offers!</h6><span className={styles.adv_price}>CHF 30</span><p>off on your next tattoo</p><span className={styles.adv_btn_wrap}><a href="#" className={styles.btn_secondary}>Refer and Earn</a></span><img src="./pexels-cottonbro-studio-5320059-3.png" alt="Tattoo now Pay later" /></div></div></div>
             ) : (            
               <div className={styles.listing_gridItem }>                   
                 <Image
