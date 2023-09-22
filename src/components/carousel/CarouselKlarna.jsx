7963ec29e17dcb76cb0f56f7b7ac1552be5e985a
Vidya K <vidya.ki@pitsolutions.com>
2023-09-22 13:41:25 +0530

    Fix latest code issue in carousal

--- src/components/carousel/CarouselKlarna.jsx
+++ src/components/carousel/CarouselKlarna.jsx
@@ -4,8 +4,10 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const CarouselKlarna = ({ items}) => {
   return (
-    <Carousel  preventMovementUntilSwipeScrollTolerance={true}
-    swipeScrollTolerance={50} >
+    <Carousel
+    preventMovementUntilSwipeScrollTolerance={true}
+    swipeScrollTolerance={50}
+  >
       {items.map((item, index) => item.image?(
         <div key={index} className='custom_carousel_slider'>
           <div className='custom_option_block'>
