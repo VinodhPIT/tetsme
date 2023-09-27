42dfbb4bf7000543f9aa546718496d4c1fdfedc6
VinodhPIT <vinodh.mn@pitsolutions.com>
2023-09-27 22:32:33 +0530

    fix/SwipperIssue

--- src/components/slider/ImageSwiper.js
+++ src/components/slider/ImageSwiper.js
@@ -1,70 +1,47 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css"; // Import the CSS file from your styles directory
-import SwiperCore, {
-  EffectCoverflow,
-  Pagination,
+import SwiperCore, { EffectCoverflow, Pagination, Autoplay } from "swiper";
 
-  Autoplay,
-} from "swiper";
-
-SwiperCore.use([EffectCoverflow, Pagination,Autoplay]);
+SwiperCore.use([EffectCoverflow, Pagination, Autoplay]);
 const images = ["./dragon.png", "./egyptian.jpeg", "./butterfly.jpeg"];
 const ImageSwiper = () => {
   const params = {
     autoHeight: false,
-    
+
     pagination: { clickable: true },
-    preventInteractionOnTransition:true,
+    preventInteractionOnTransition: true,
     grabCursor: true,
     centeredSlides: true,
     intialSlide: 1,
     slidesPerView: 1.5,
     loop: true,
-   speed:150,
-    
+    speed: 150,
+    simulateTouch: false,
+
     effect: "coverflow",
     coverflowEffect: {
       rotate: 0,
       depth: 200,
       slideShadows: false,
-      modifier:1
+      modifier: 1,
     },
 
     pagination: {
       el: ".swiper-pagination",
-      dynamicBullets:false,
-      
+      dynamicBullets: false,
+
       clickable: true,
     },
-    autoplay:{
+    autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
     breakpoints: {
-      320: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      414: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      560: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
       768: {
         slidesPerView: 1.5,
-        spaceBetween: 20,
-      },
-      820: {
-        slidesPerView: 1.5,
         spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
+        simulateTouch: true,
       },
     },
   };
